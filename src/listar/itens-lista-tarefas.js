09a017f47814716e88966800071958d7c88dbfb0
Daniella Dutra <danivetdutra@gmail.com>
2022-01-17 14:44:53 -0300

    Criado html do item da lista de tarefas

--- src/listar/itens-lista-tarefas.js
+++ src/listar/itens-lista-tarefas.js
@@ -1,14 +1,19 @@
 import PropTypes from 'prop-types';
-import { FontAwsemoIcon } from '@fortawesome/react-fontawesome'
-import { faEdit } from '@fortawesome/free-solid-svg-icons'
+import { FontAwsemoIcon } from '@fortawesome/react-fontawesome';
+import { faEdit } from '@fortawesome/free-solid-svg-icons';
 import { useHistory } from 'react-router-dom';
 
-
 function ItensListaTarefas(props) {
-  return()
+  return props.tarefas.map((tarefa) => (
+    <tr key={tarefa.id} data-testid="tarefa  ">
+      <td width="75%" data-testid="nome-tarefa">
+        {tarefa.nome}
+      </td>
+    </tr>
+  ));
 }
 
-ItensListaTarefas.prototype = {
+ItensListaTarefas.propTypes = {
   tarefas: PropTypes.array.isRequired,
   recarregarTarefas: PropTypes.func.isRequired,
 };
