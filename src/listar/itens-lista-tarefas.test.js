0defdbbd26efd17a9b53628bca976fbcd8c57538
Daniella Dutra <danivetdutra@gmail.com>
2022-01-17 16:47:40 -0300

    Teste unitario criado para componente de itens de tarefas

--- src/listar/itens-lista-tarefas.test.js
+++ src/listar/itens-lista-tarefas.test.js
@@ -1,1 +1,13 @@
+import ReactDOM from 'react-dom';
+import ItensListaTarefas from './itens-lista-tarefas';
 
+describe('Teste do componente que exibe um item da listagem de tarefas', () => {
+  it('deve redenrizar o componente sem erros', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(
+      <ItensListaTarefas tarefas={[]} recarregarTarefas={() => false} />,
+      div
+    );
+    ReactDOM.unmountComponentAtNode(div);
+  });
+});
