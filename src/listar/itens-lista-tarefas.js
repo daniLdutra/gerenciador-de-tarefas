f4e9bf653759bfb66aa25563a680edce8517d0d5
Daniella Dutra <danivetdutra@gmail.com>
2022-01-17 15:14:55 -0300

    Botao criado para identificar tarefa concluida

--- src/listar/itens-lista-tarefas.js
+++ src/listar/itens-lista-tarefas.js
@@ -1,13 +1,24 @@
 import PropTypes from 'prop-types';
-import { FontAwsemoIcon } from '@fortawesome/react-fontawesome';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEdit } from '@fortawesome/free-solid-svg-icons';
 import { useHistory } from 'react-router-dom';
+import { Button } from 'react-bootstrap';
 
 function ItensListaTarefas(props) {
+  const history = useHistory();
+
   return props.tarefas.map((tarefa) => (
     <tr key={tarefa.id} data-testid="tarefa  ">
       <td width="75%" data-testid="nome-tarefa">
         {tarefa.nome}
+      </td>
+      <td className="text-right">
+        <Button
+          onClick={() => history.push('/atualizar/' + tarefa.id)}
+          className={tarefa.concluida ? 'hidden' : 'btn btn-warning btn-sm'}
+        >
+          <FontAwesomeIcon icon={faEdit} />
+        </Button>
       </td>
     </tr>
   ));
