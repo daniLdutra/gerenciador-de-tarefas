d2c9be5aaf38176f3a6cd9e76dd2c85b6261f213
Daniella Dutra <danivetdutra@gmail.com>
2022-01-18 11:16:45 -0300

    Criado botoes e modal para conclusao da tarefa

---
+++ src/listar/concluir-tarefa.js
@@ -0,0 +1,71 @@
+import { useState } from 'react';
+import Proptypes from 'prop-types';
+import {
+  Modal,
+  Button,
+  ModalTitle,
+  ModalBody,
+  ModalFooter,
+} from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faClipboardCheck } from '@fortawesome/free-solid-svg-icons';
+import ModalHeader from 'react-bootstrap/esm/ModalHeader';
+
+function ConcluirTarefa(props) {
+  const [exibirModal, setExibirModal] = useState(false);
+
+  function handleAbrirModal(event) {
+    event.preventDefault();
+    setExibirModal(true);
+  }
+
+  function handleFecharModal() {
+    setExibirModal(false);
+  }
+
+  return (
+    <span className={props.className}>
+      <Button
+        className="btn-sm"
+        onClick={handleAbrirModal}
+        data-testid="btn-abrir-modal"
+      >
+        <FontAwesomeIcon icon={faClipboardCheck} />
+      </Button>
+      <Modal show={exibirModal} onHide={handleFecharModal} data-testid="modal">
+        <ModalHeader closeButton>
+          <ModalTitle>Concluir Tarefa</ModalTitle>
+        </ModalHeader>
+        <ModalBody>
+          Deseja realmente concluir a seguinte tarefa?
+          <br />
+          <strong>{props.tarefa.nome}</strong>
+        </ModalBody>
+        <ModalFooter>
+          <Button
+            variant="primary"
+            onClick={handleConcluirTarefa}
+            data-testid="btn-concluir"
+          >
+            Sim
+          </Button>
+          <Button
+            variant="light"
+            onClick={handleFecharModal}
+            data-testid="btn-fechar-modal"
+          >
+            Não
+          </Button>
+        </ModalFooter>
+      </Modal>
+    </span>
+  );
+}
+
+ConcluirTarefa.prototype = {
+  tarefa: Proptypes.object.isRequired,
+  recarregarTarefas: Proptypes.func.isRequired,
+  className: Proptypes.string.isRequired,
+};
+
+export default ConcluirTarefa;
--- src/listar/itens-lista-tarefas.test.js
+++ src/listar/itens-lista-tarefas.test.js
@@ -3,7 +3,6 @@ import ItensListaTarefas from './itens-lista-tarefas';
 import Tarefa from '../models/tarefa.models';
 import { render, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
-import { get } from 'hookrouter';
 
 describe('Teste do componente que exibe um item da listagem de tarefas', () => {
   const nomeTarefa = 'Tarefa';
